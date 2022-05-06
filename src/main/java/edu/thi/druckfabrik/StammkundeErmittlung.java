36aae2b7aadda98561fffce0f949db377d155892
maw0979 <maw0979@thi.de>
2022-05-06 20:48:07 +0200

    Änderunge in der Stammkunde ermittlung. Kurz gesagt alles vorherige zurückgesetzt

--- src/main/java/edu/thi/druckfabrik/StammkundeErmittlung.java
+++ src/main/java/edu/thi/druckfabrik/StammkundeErmittlung.java
@@ -9,11 +9,11 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 import edu.thi.druckfabrik.beans.KundenBean;
 
-public class AnfrageToBean implements JavaDelegate{
+public class StammkundeErmittlung implements JavaDelegate{
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
-			KundenBean kunde = new KundenBean();
+			/*KundenBean kunde = new KundenBean();
 			kunde.setAnrede((String) execution.getVariable("anrede"));
 			kunde.setVorname((String) execution.getVariable("vorname"));
 			kunde.setNachname((String) execution.getVariable("nachname"));
@@ -23,23 +23,25 @@ public class AnfrageToBean implements JavaDelegate{
 			kunde.setPostleitzahl((String) execution.getVariable("postleitzahl"));
 			kunde.setStadt((String) execution.getVariable("stadt"));
 			kunde.setTelefonnummer((String) execution.getVariable("telefonnummer")) ;
-			kunde.setStammkunde(false);
+			kunde.setStammkunde(false);*/
 			
 //Hier erfolgt eine Ueberpruefung ob der Kunde ein Stammkunde ist. Ueberprueft wird anhand eines dummy files			
+			String email = (String) execution.getVariable("email");
+			Boolean stammkunde = false;
 			File file = new File("../standalone/tmp/stammkunde.txt");	
 			
 			try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 			    String line;
 			    while ((line = br.readLine()) != null) {
-			    	if(!line.strip().isEmpty() && line.strip().equals(kunde.getEmail())) {
-			    		kunde.setStammkunde(true);
+			    	if(!line.strip().isEmpty() && line.strip().equals(email)) {
+			    		stammkunde = true;
 			    	}
 			    }
 			    
 			}
 			
 			
-			execution.setVariable("kunde", kunde);
+			execution.setVariable("stammkunde", stammkunde);
 		
 	}
 	
