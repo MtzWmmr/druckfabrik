ccc91caaa79b82543793da6b07089ec213b8522e
Matthias <maw0979@thi.de>
2022-05-09 21:35:25 +0200

    Mail wieder zurückgebaut. Es wird keine Mail mehr versendet sondern nur noch auf der console ausgegeben

--- src/main/java/edu/thi/druckfabrik/RueckfrageMail.java
+++ src/main/java/edu/thi/druckfabrik/RueckfrageMail.java
@@ -23,6 +23,35 @@ import javax.mail.internet.MimeMessage;
 public class RueckfrageMail implements JavaDelegate{
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
+		//am 30.05.2022 wurde diese Möglichkeit E-Mails über Google Mail zu schreiben aus Sicherheitsgründen entfernt, deshalb lediglich ausgabe in der Console
+		String reason = (String) execution.getVariable("rueckfragegrund");
+		String text = (String) execution.getVariable("rueckfragetext");
+		String anrede = (String) execution.getVariable("anrede");
+		String name = (String) execution.getVariable("nachname");
+		String email = (String) execution.getVariable("email");
+		Date startdate = (Date) execution.getVariable("startdate");
+		String hours = String.valueOf(startdate.getHours());
+		String minutes = String.valueOf(startdate.getMinutes());
+		
+		LocalDate datum = startdate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+		int day = datum.getDayOfMonth();
+		int month = datum.getMonthValue();
+		int year = datum.getYear();
+		
+		String betreff = "Rückfrage aus Grund: " + reason;
+		
+		StringBuilder nachricht = new StringBuilder();
+		nachricht.append("Hallo " + anrede +" " + name + ",\n");
+		nachricht.append("Vielen Dank für Ihre Anfrage vom " + day + "." + month + "." + year + " " + minutes + ":" + hours + ".\n");
+		nachricht.append(text);
+		nachricht.append("\n\nViele Grüße \nIhr Druckfabrik-Team");
+		
+		System.out.println("Mail To: " + email);
+		System.out.println("Betreff: " + betreff);
+		System.out.println("Nachricht: " + nachricht.toString());
+
+	}
+	/*public void execute(DelegateExecution execution) throws Exception {
 		String reason = (String) execution.getVariable("rueckfragegrund");
 		String text = (String) execution.getVariable("rueckfragetext");
 		String anrede = (String) execution.getVariable("anrede");
@@ -51,10 +80,10 @@ public class RueckfrageMail implements JavaDelegate{
 		
 		
 	}
-	
-/*	 
-* Folgender Code ist von Christian Ullenboom java 7- Mehr als eine Insel Ã¼bernommen https://openbook.rheinwerk-verlag.de/java7/1507_11_012.html	
-*/		
+		  
+
+	//Folgender Code ist von Christian Ullenboom java 7- Mehr als eine Insel Ã¼bernommen https://openbook.rheinwerk-verlag.de/java7/1507_11_012.html	
+		
 	
 	public static Session getGMailSession( String user, String pass ){
 	    final Properties props = new Properties();
@@ -85,5 +114,5 @@ public class RueckfrageMail implements JavaDelegate{
 	msg.setSubject( subject );
 	msg.setContent( message, "text/plain" );
 	Transport.send( msg );
-	}
+	}*/
 }
