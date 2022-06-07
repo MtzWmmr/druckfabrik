db086113f2c5e6b379a380a4e5db5eeb79e21fef
Simon Kreutel <sik7467@thi.de>
2022-06-07 11:39:47 +0200

    Umsetzung des Adhoc-TP im paketVersandfertigMachen TP

---
+++ src/main/java/edu/thi/druckfabrik/BenutzeraufgabenUeberspringen.java
@@ -0,0 +1,24 @@
+package edu.thi.druckfabrik;
+
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.delegate.TaskListener;
+import org.camunda.bpm.engine.impl.TaskServiceImpl;
+import org.camunda.bpm.engine.impl.interceptor.CommandExecutorImpl;
+
+public class BenutzeraufgabenUeberspringen implements TaskListener{
+	
+	@Override
+	public void notify (DelegateTask delegateTask) {
+		if ((Boolean) delegateTask.getExecution().getVariable("completionCondition")) {
+			TaskServiceImpl taskService = new TaskServiceImpl();
+			taskService.setCommandExecutor(new CommandExecutorImpl());
+			this.finishCurrentTask(taskService, delegateTask);
+		}
+	}
+	
+	private void finishCurrentTask (TaskService taskService, DelegateTask currentTask) {
+		taskService.complete(currentTask.getId());
+	}
+
+}
