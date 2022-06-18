65cc6970e414dbf45fdd9623cc95fe79ef8cd9e7
Simon Kreutel <sik7467@thi.de>
2022-06-18 16:41:48 +0200

    Hinzufügen von Datenobjekten. Ausgabe von Angebotsdaten und Versandadresse an den jeweiligen Stellen in der Eclipse Konsole.

--- src/main/java/edu/thi/druckfabrik/servicetasks/AngebotDrucken.java
+++ src/main/java/edu/thi/druckfabrik/servicetasks/AngebotDrucken.java
@@ -6,13 +6,19 @@ package edu.thi.druckfabrik.servicetasks;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+import edu.thi.druckfabrik.beans.AngebotBean;
+
 
 public class AngebotDrucken implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
+		AngebotBean angebot = (AngebotBean) execution.getVariable("angebot");
 		System.out.println("Angebot drucken");
 		System.out.println("Das Angebot wird gedruckt...");
+		System.out.println("Preis pro Stück bei Bestellmenge ab 1 Stück: " + angebot.getPreis1() + "0€");
+		System.out.println("Preis pro Stück bei Bestellmenge ab 100 Stück: " + angebot.getPreis100() + "0€");
+		System.out.println("Preis pro Stück bei Bestellmenge ab 1000 Stück: " + angebot.getPreis1000() + "0€");
 		System.out.println("Das Angebot wurde erfolgreich gedruckt.");
 		
 		
--- src/main/java/edu/thi/druckfabrik/servicetasks/VersandetikettDrucken.java
+++ src/main/java/edu/thi/druckfabrik/servicetasks/VersandetikettDrucken.java
@@ -6,13 +6,22 @@ package edu.thi.druckfabrik.servicetasks;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+import edu.thi.druckfabrik.beans.KundenBean;
+
 
 public class VersandetikettDrucken implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
+		KundenBean kunde = (KundenBean) execution.getVariable("kunde");
 		System.out.println("Versandetikett drucken");
 		System.out.println("Das Versandetikett wird gedruckt...");
+		System.out.println("Vorname: " + kunde.getVorname());
+		System.out.println("Nachname: " + kunde.getNachname());
+		System.out.println("Straße: " + kunde.getStrasse());
+		System.out.println("Hausnummer: " + kunde.getHausnummer());
+		System.out.println("Postleitzahl: " + kunde.getPostleitzahl());
+		System.out.println("Stadt: " + kunde.getStadt());
 		System.out.println("Das Versandetikett wurde erfolgreich gedruckt.");
 		
 		
