e25b96c3d8516a61a45f5363ce138165906bbffc
Simon Kreutel <sik7467@thi.de>
2022-06-30 21:53:20 +0200

    Erlösung

--- src/main/java/edu/thi/druckfabrik/servicetasks/ObjektDrucken.java
+++ src/main/java/edu/thi/druckfabrik/servicetasks/ObjektDrucken.java
@@ -5,12 +5,16 @@ package edu.thi.druckfabrik.servicetasks;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
+import edu.thi.druckfabrik.beans.PrototypBean;
+
 
 public class ObjektDrucken implements JavaDelegate {
 
 	@Override
 	public void execute(DelegateExecution execution) throws Exception {
+		PrototypBean prototyp = (PrototypBean) execution.getVariable("prototyp");
 		System.out.println("Druckvorgang");
+		System.out.println("Maße in mm: " + prototyp.getHoehe() + " x " + prototyp.getBreite() + " x " + prototyp.getLaenge() + " (H*B*L)");
 		System.out.println("Das Objekt wird gedruckt...");
 		System.out.println("Das Objekt wurde erfolgreich gedruckt. Sie können es nun entnehmen.");
 		
